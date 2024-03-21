edfda4f6a6db37bda13d64d744326056c7824e11
GlebKasi <glebk-vrn@yandex.ru>
2024-03-21 11:27:06 +0300

    шим переделал

--- 4_lecture/2_task/pwm.v
+++ 4_lecture/2_task/pwm.v
@@ -5,21 +5,12 @@ module pwm(
 );
 reg [2:0]n;
 always @(posedge clk)
-	if(reset)
-	begin
-		pwm_signal <= 0;
-		n <= 0;
-	end
-	else
-		if(n == 4)
-		begin
-			pwm_signal <= 1;
-			n <= 0;
-		end
-		else
-		begin
-			pwm_signal <= 0;
-			n <= n + 1;
-		end
+begin
+	pwm_signal <= (reset) ? 0 : (n == 4) ? 1 : 0;
+end
 
+always @(posedge clk)
+begin
+	n <= (reset) ? 0 : (n == 4) ? 0 : n + 1;
+end
 endmodule
